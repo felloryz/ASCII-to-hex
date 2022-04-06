25c055c5fce0fa9f3bacff46d766bfbb712fb1d2
felloryz <felloryz@gmail.com>
2022-04-06 16:58:04 +0300

    Update code appearance

--- asciitohex.c
+++ asciitohex.c
@@ -1,27 +1,36 @@
 #include <stdio.h>
 #include <string.h>
 
+void StrToHex(char *string, unsigned int *hexArray);
+
 int main(void)
 {
     char str[80]; // ASCII string
-    unsigned int num[80]; // hex array 
+    unsigned int hex[80]; // hex array 
     
     scanf("%s", str);
 
-    int len = strlen(str);
+    StrToHex(str, hex);
+
+    for (int i = 0; i < (strlen(str) >> 1); i++)
+    {
+        printf("%02x  ", hex[i]);
+    }
+    
+    return 0;
+}
+
+void StrToHex(char *string, unsigned int *hexArray)
+{
+    int len = strlen(string);
     if ((len & 1) != 0)
+    {
         len++;
+    }
 
     int i, j;
     for (i = 0, j = 0; j < len, i < (len >> 1) ; ++i, j+=2)
     {
-        sscanf(&str[j], "%02x", &num[i]);
+        sscanf(&string[j], "%02x", &hexArray[i]);
     }
-
-    for (i = 0; i < (len >> 1); i++)
-    {
-        printf("%02x  ", num[i]);
-    }
-    
-    return 0;
 }
